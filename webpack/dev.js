d4e0f9931b724f0933d92741699439b0c2105ff2
Akash <subs@appsparkler.com>
2019-03-15 07:53:24 +0530

    first commit

---
+++ webpack/dev.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+console.log(path.join(__dirname, '../'))
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'index_bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '../src/index.html')
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, '../dist'),
+        bonjour: true,
+        overlay: true,
+        watchOptions: {
+            ignored: /.*\/node_modules\/.*/
+        },
+        stats: {
+            assets: false,
+            cached: false,
+            cachedAssets: false,
+            children: false,
+            chunks: false,
+            chunkModules: false,
+            chunkOrigins: false,
+            colors: true,
+            depth: false,
+            entrypoints: true,
+            excludeAssets: /app\/assets/,
+            hash: false,
+            maxModules: 15,
+            modules: false,
+            performance: true,
+            reasons: false,
+            source: false,
+            timings: true,
+            version: false,
+            warnings: true,
+        }
+    }
+}
